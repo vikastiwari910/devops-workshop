9385f331e3f3d9c0a606e848de7192c71a0b90de
vikas <vikas910@yahoo.com>
2023-09-04 17:43:02 +0530

    ansible setup added

--- v1-ec2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region= "us-east-1"
-}
-
- resource "aws_instance" "test" {
-  ami           = "ami-051f7e7f6c2f40dc1"
-  instance_type = "t2.micro"
-  key_name       = "namo"
- }
